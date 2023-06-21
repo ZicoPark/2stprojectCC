3b993477113095f7ebecda33c2b9b296a86293da
uumim <asd324500@naver.com>
2023-06-21 16:31:57 +0900

    gnb수정, 프로젝트 등록, 리스트 확인

--- src/main/java/kr/co/cc/project/service/ProjectService.java
+++ src/main/java/kr/co/cc/project/service/ProjectService.java
@@ -1,5 +1,42 @@
 package kr.co.cc.project.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import kr.co.cc.project.dao.ProjectDAO;
+import kr.co.cc.project.dto.ProjectDTO;
+
+@Service
+@MapperScan(value= {"kr.co.cc.project.dao"})
 public class ProjectService {
+
+	@Autowired ProjectDAO dao;
+	
+	Logger logger = LoggerFactory.getLogger(getClass());
+	
+	public String write(HashMap<String, String> params) {
+		ProjectDTO dto = new ProjectDTO();
+		dto.setName(params.get("name"));
+		dto.setMember_id(params.get("member_id"));
+		dto.setPublic_range(Integer.valueOf(params.get("public_range")));
+		dto.setPriod(params.get("priod"));
+		dto.setDeadlinepriod(params.get("deadlinepriod"));
+		
+		int row = dao.write(dto);
+		logger.info("update row : " + row);
+		String page = "redirect:/projects.go";
+		return page;
+	}
+
+	public ArrayList<ProjectDTO> list() {
+		
+		return dao.list();
+	}
 
 }
