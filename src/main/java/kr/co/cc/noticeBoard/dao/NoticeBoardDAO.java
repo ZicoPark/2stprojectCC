dfc0e3d9fe48b195bc3c9eb64e9c9a6ed288164f
mokjingyeong <mok5564@naver.com>
2023-06-21 16:42:57 +0900

    공지 리스트

--- src/main/java/kr/co/cc/noticeBoard/dao/NoticeBoardDAO.java
+++ src/main/java/kr/co/cc/noticeBoard/dao/NoticeBoardDAO.java
@@ -1,5 +1,13 @@
 package kr.co.cc.noticeBoard.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.co.cc.noticeBoard.dto.NoticeBoardDTO;
+
 public interface NoticeBoardDAO {
+
+	ArrayList<NoticeBoardDTO> list();
 
 }
