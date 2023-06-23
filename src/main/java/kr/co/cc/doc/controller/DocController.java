d0f8eac6085b2702a12f78b127bf1f319c7d4522
mobpolice <sbexclusive9@gmail.com>
2023-06-23 09:43:12 +0900

    서비스 컨트롤러 역할 수정

--- src/main/java/kr/co/cc/doc/controller/DocController.java
+++ src/main/java/kr/co/cc/doc/controller/DocController.java
@@ -37,23 +37,7 @@ public class DocController {
 	@RequestMapping(value="/docWriteForm.do")
 	public ModelAndView docWriteForm() {
 		
-		ModelAndView mav = new ModelAndView("docWriteForm");
-		
-		// 기안문 양식 불러오기
-		ArrayList<DocFormDTO> docFormList = service.docFormCall();
-		mav.addObject("docFormList", docFormList);
-		
-		// 결재 종류 불러오기
-		ArrayList<ApprovalDTO> approvalKindList = service.approvalKindCall();
-		// 기안은 제외(기안자가 나 자신이니까)
-		approvalKindList.remove(0);
-		mav.addObject("approvalKindList", approvalKindList);
-		
-		// 결재자 선택하기 위해 직원 리스트 불러오기
-		ArrayList<MemberDTO> memberList = service.memberListCall();
-		mav.addObject("memberList", memberList);
-		
-		return mav;
+		return service.docWriteForm();
 	}
 	
 	@RequestMapping(value="/docWrite.do")
@@ -63,10 +47,13 @@ public class DocController {
 		
 		HashMap<String, String> map = new HashMap<String, String>();
 		ArrayList<HashMap<String, String>> approvalList = new ArrayList<HashMap<String,String>>();
+		
+		// 결재선 정렬 로직
 		for(int i=0;i<approvalVariable.length;i++) {
 			map.put(approvalVariable[i], approvalPerson[i]);
 			approvalList.add(map);
 		}
+		
 		service.docWrite(params, approvalList, attachment);
 		
 		return null;
--- src/main/java/kr/co/cc/doc/service/DocService.java
+++ src/main/java/kr/co/cc/doc/service/DocService.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import kr.co.cc.doc.dao.DocDAO;
 import kr.co.cc.doc.dto.ApprovalDTO;
@@ -26,6 +27,27 @@ public class DocService {
 		this.dao = dao;
 	}
 
+
+	public ModelAndView docWriteForm() {
+		ModelAndView mav = new ModelAndView("docWriteForm");
+		
+		// 기안문 양식 불러오기
+		ArrayList<DocFormDTO> docFormList = docFormCall();
+		mav.addObject("docFormList", docFormList);
+		
+		// 결재 종류 불러오기
+		ArrayList<ApprovalDTO> approvalKindList = approvalKindCall();
+		// 기안은 제외(기안자가 나 자신이니까)
+		approvalKindList.remove(0);
+		mav.addObject("approvalKindList", approvalKindList);
+		
+		// 결재자 선택하기 위해 직원 리스트 불러오기
+		ArrayList<MemberDTO> memberList = memberListCall();
+		mav.addObject("memberList", memberList);
+		
+		return mav;
+	}
+	
 	public ArrayList<DocFormDTO> docFormCall() {
 
 		return dao.docFormCall();
@@ -53,5 +75,6 @@ public class DocService {
 		
 		
 	}
+
 	
 }
