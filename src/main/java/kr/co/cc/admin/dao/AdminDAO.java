c8693b285a060e971e7543a0e6461faed9384f2a
chltmddnjs <clickq345@naver.com>
2023-07-03 17:37:55 +0900

    코드수정

--- src/main/java/kr/co/cc/admin/dao/AdminDAO.java
+++ src/main/java/kr/co/cc/admin/dao/AdminDAO.java
@@ -8,11 +8,17 @@ public interface AdminDAO {
 
 	ArrayList<AdminDTO> MemberList();
 
-	AdminDTO AdminMemberDetail(String id);
+	AdminDTO AdminMemberDetail(String user_id);
 
 	void MemberUpdate(boolean admin, String dept, String job, String status);
 
-	int MemberUpdate(AdminDTO dto, String id);
+	int MemberUpdate(AdminDTO dto, String user_id);
+
+	ArrayList<AdminDTO> MemberONOFFList();
+
+	AdminDTO MemberONOFFListDetail(String id);
+
+	int MemberONOFFDelete(String id);
 
 	
 
