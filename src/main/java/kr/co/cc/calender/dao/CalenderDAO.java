d00a33722b3710b04c517e3d0e7b70cf5e9b3990
chltmddnjs <clickq345@naver.com>
2023-07-06 14:17:17 +0900

    uuid수정

--- src/main/java/kr/co/cc/calender/dao/CalenderDAO.java
+++ src/main/java/kr/co/cc/calender/dao/CalenderDAO.java
@@ -1,5 +1,13 @@
 package kr.co.cc.calender.dao;
 
+import java.util.ArrayList;
+
+import kr.co.cc.calender.dto.CalenderDTO;
+
 public interface CalenderDAO {
+
+	ArrayList<CalenderDTO> CalenderList();
+
+	ArrayList<CalenderDTO> CalenderListSave(String event);
 
 }
